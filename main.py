512c5d79ef6c56c54aec1b1620a8d73dbdcb52e5
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-16 11:24:52 -0600

    Designed first golf hole

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import pygame
 
 pygame.init()
 
+COLOR_ROUGH = (16, 122, 39)
+COLOR_FAIRWAY = (16, 163, 48)
+COLOR_TEE = (155, 155, 155)
+COLOR_HOLE = (82, 82, 82)
+
 screen = pygame.display.set_mode((800,600))
 
 while True:
@@ -11,7 +16,12 @@ while True:
         if event.type == pygame.QUIT:
             quit()
 
-    screen.fill((0,255,0))
+    screen.fill(COLOR_ROUGH)
+
+    pygame.draw.ellipse(screen, COLOR_FAIRWAY, [340,100,120,400])
+    pygame.draw.rect(screen, COLOR_TEE, [390, 480, 20, 50])
+    pygame.draw.ellipse(screen, COLOR_HOLE, [390, 120, 20, 20])
+
     pygame.display.update()
 
 pygame.quit()
