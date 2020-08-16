3670bc5587af852dd138a95e83500f676fe9431c
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-16 11:10:54 -0600

    Wrote simple starter PyGame program

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 #!/usr/local/bin/python3
 
-print("Hello, world!")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit()
+
+    screen.fill((0,255,0))
+    pygame.display.update()
+
+pygame.quit()
