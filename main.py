Fixed scaling and compass angle

main.py
@@ -72,16 +72,16 @@ class Hole():
         
 class Wind(Circle):
     def generateWind(self):
-        self.speed     = np.random.rand() * float(10)
-        self.direction = np.deg2rad(np.random.randint(360))
-        self.vx = self.speed * np.cos(self.direction)
-        self.vy = self.speed * np.sin(self.direction)
+        self.speed     = np.random.rand() * float(100)
+        self.heading = np.deg2rad(np.random.randint(360))
+        self.vx = self.speed * np.cos(self.heading)
+        self.vy = self.speed * np.sin(self.heading)
     def drawCompass(self):
         Circle.draw(self,(201, 191, 189))
         needleColor = (255, 255, 255)
         start_pos = (self.x + self.radius * 0.5,self.y + self.radius * 0.5)
-        end_pos = (start_pos[0] - 0.5 * self.radius * np.cos(self.direction), 
-                   start_pos[1] - 0.5 * self.radius * np.sin(self.direction))
+        end_pos = (start_pos[0] + 0.5 * self.radius * np.cos(self.heading), 
+                   start_pos[1] + 0.5 * self.radius * np.sin(self.heading))
         width = 5
         pygame.draw.line(screen, needleColor, start_pos, end_pos, width)
         self.message_display(('Wind Speed: ' + str(round(self.speed,1))))
@@ -89,15 +89,15 @@ class Wind(Circle):
         textSurface = font.render(text, True, (255, 255, 255))
         return textSurface, textSurface.get_rect()
     def message_display(self,text):
-        largeText = pygame.font.Font('freesansbold.ttf',16)
-        TextSurf, TextRect = self.text_objects(text, largeText)
+        windSpeedText = pygame.font.Font('freesansbold.ttf',16)
+        TextSurf, TextRect = self.text_objects(text, windSpeedText)
         TextRect.center = (self.x + self.radius * 0.5, self.y - 15)
         screen.blit(TextSurf, TextRect)
         
     vx: float = 0.0
     vy: float = 0.0
     speed: float = 0.0
-    direction: float = 0.0
+    heading: float = 0.0
         
 hole = Hole()
 disc = Disc(395, 500, 10)
@@ -123,7 +123,7 @@ while running:
         mouse_down = False
         mouse_movement = pygame.mouse.get_rel()
         disc.vx = mouse_movement[0] * 2 + wind.vx
-        disc.vy = mouse_movement[1] * 2 + wind.vy
+        disc.vy = mouse_movement[1] * 2 + wind.vy        
         stroke_count = stroke_count + 1
 
     if (not disc.hit(basket)):