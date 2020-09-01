4dfd508abe9bbb7cd92071c28674d914f885f130
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-08-31 23:12:48 -0600

    Whitespace fixes

--- color.py
+++ color.py
@@ -1,12 +1,12 @@
 BLACK       = (255, 255, 255)
 
 LIGHT_GREY  = (185, 185, 185)
-GREY        = (82, 82, 82)
+GREY        = ( 82,  82,  82)
 
-DARK_GREEN  = (11, 61, 17)
-GREEN       = (16, 122, 39)
-LIGHT_GREEN = (16, 163, 48)
+DARK_GREEN  = ( 11,  61,  17)
+GREEN       = ( 16, 122,  39)
+LIGHT_GREEN = ( 16, 163,  48)
 
-RED         = (176, 23, 12)
+RED         = (176,  23,  12)
 
-YELLOW      = (227, 220, 32)
+YELLOW      = (227, 220,  32)
