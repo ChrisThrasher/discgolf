f8af384a703ca5efd88ee75aca508054dbd5d8b8
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-09-06 23:25:06 -0600

    Wrote utilities for vector operations

--- vec2.py
+++ vec2.py
@@ -1,6 +1,14 @@
+import math
+
 class Vec2:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    def __sub__(self, rhs):
+        return Vec2(self.x - rhs.x, self.y - rhs.y)
+    def norm(self):
+        return math.sqrt(self.norm2())
+    def norm2(self):
+        return pow(self.x, 2) + pow(self.y, 2)
     x: int
     y: int
