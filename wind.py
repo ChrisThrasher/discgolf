a70b4a2f13e4a213ad5409ca93053a66050971ee
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-09-07 20:43:16 -0600

    Removed function that didn't touch internal state

--- wind.py
+++ wind.py
@@ -13,17 +13,14 @@ class Wind(Circle):
         self.speed = np.random.rand() * max_speed
         self.heading = np.deg2rad(np.random.randint(360))
         self.vel = Vec2(np.cos(self.heading), np.sin(self.heading)) * self.speed
-    def text_objects(self, text, font):
-        text_surface = font.render(text, True, color.WHITE)
-        return text_surface, text_surface.get_rect()
     def draw(self):
         super().draw()
         start_pos = self.pos
         end_pos = self.pos + Vec2(np.cos(self.heading), np.sin(self.heading)) * self.radius
         pygame.draw.line(screen, color.WHITE, start_pos.as_tuple(), end_pos.as_tuple(), width=5)
-        wind_speed_text = pygame.font.Font('freesansbold.ttf', 16)
         text = ('Wind Speed: ' + str(round(self.speed, 1)))
-        text_surf, text_rect = self.text_objects(text, wind_speed_text)
+        text_surf = pygame.font.Font('freesansbold.ttf', 16).render(text, True, color.WHITE)
+        text_rect = text_surf.get_rect()
         text_rect.center = (int(self.pos.x), int(self.pos.y - self.radius - 15))
         screen.blit(text_surf, text_rect)
     speed: float = 0.0
