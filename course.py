753720717c0437e9bfc1907fb7de4354a77c4acb
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-09-11 22:33:23 -0600

    Added new data structures for holes and other subitems

--- course.py
+++ course.py
@@ -2,19 +2,58 @@ import pygame
 
 import color
 
+from circle import Circle
 from screen import screen
+from vec2 import Vec2
 
-def DrawHole1():
-    screen.fill(color.GREEN)
-    pygame.draw.ellipse(screen, color.LIGHT_GREEN, [340, 100, 120, 400])
-    pygame.draw.rect(screen, color.LIGHT_GREY, [390, 480, 20, 50])
+class Fairway:
+    def __init__(self, pos, width, height):
+        self.pos = pos
+        self.width = width
+        self.height = height
+    def draw(self):
+        pygame.draw.ellipse(screen, color.LIGHT_GREEN, [self.pos.x, self.pos.y, self.width, self.height])
+
+class Tee:
+    def __init__(self, pos):
+        self.pos = pos
+    def draw(self):
+        pygame.draw.rect(screen, color.LIGHT_GREY, [self.pos.x, self.pos.y, 20, 50])
+
+class Hole:
+    def __init__(self, fairway, tee, trees, basket):
+        self.fairway = fairway
+        self.tee = tee
+        self.trees = trees
+        self.basket = basket
+    def reset(self, disc):
+        disc.pos = self.tee.pos
+    def draw(self):
+        self.fairway.draw()
+        self.tee.draw()
+        for tree in self.trees:
+            tree.draw()
+        self.basket.draw()
 
 class Course:
-    def __init__(self):
-        self.current_hole = 1
-        self.holes = {1 : DrawHole1}
+    def __init__(self, holes):
+        self.current_hole = 0
+        self.holes = holes
+    def hole(self):
+        return self.holes[self.current_hole]
     def next_hole(self):
         self.current_hole = self.current_hole + 1
-        return self.current_hole <= len(self.holes)
+        return self.current_hole < len(self.holes)
     def draw(self):
-        self.holes[self.current_hole]()
+        screen.fill(color.GREEN)
+        self.holes[self.current_hole].draw()
+
+fairway1 = Fairway(Vec2(340, 100), 120, 400)
+tee1 = Tee(Vec2(390, 480))
+trees1 = [Circle(Vec2(450, 300), 5, color.DARK_GREEN),
+          Circle(Vec2(400, 350), 5, color.DARK_GREEN),
+          Circle(Vec2(350, 300), 5, color.DARK_GREEN)]
+basket1 = Circle(Vec2(400, 120), 10, color.GREY)
+hole1 = Hole(fairway1, tee1, trees1, basket1)
+
+COURSE = Course([hole1])
--- main.py
+++ main.py
@@ -12,20 +12,15 @@ from wind import Wind
 from bag import BAG
 from vec2 import Vec2
 from mouse import Mouse
-from course import *
+from course import COURSE
 
 pygame.init()
 clock = pygame.time.Clock()
 stroke_count = 0
 mouse = Mouse()
 
-basket = Circle(Vec2(400, 120), 10, color.GREY)
-trees = [Circle(Vec2(450, 300), 5, color.DARK_GREEN),
-         Circle(Vec2(400, 350), 5, color.DARK_GREEN),
-         Circle(Vec2(350, 300), 5, color.DARK_GREEN)]
 wind = Wind(Vec2(100, 100), 50, max_speed=50)
 disc = Disc(Vec2(400, 500), 5, color=BAG[0].color, resistance_coef=BAG[0].resistance_coef)
-course = Course()
 
 while True:
     # Track mouse position at all times
@@ -48,14 +43,14 @@ while True:
             stroke_count = stroke_count + 1
 
     # Update disc
-    if not disc.hit(basket):
+    if not disc.hit(COURSE.hole().basket):
         disc.update_velocity(wind)
         if disc.speed() < 15:
             disc.stop()
         disc.update_position()
     else:
         print("Completed the hole in", stroke_count, "strokes.")
-        if not course.next_hole():
+        if not COURSE.next_hole():
             break
 
     if disc.off_screen():
@@ -63,17 +58,13 @@ while True:
         break
 
     # Detect obstacle collisions
-    for tree in trees:
+    for tree in COURSE.hole().trees:
         if disc.hit(tree):
             disc.vel = Vec2(0.0, 0.0)
 
     # Draw objects
-    course.draw()
-    basket.draw()
+    COURSE.draw()
     wind.draw()
-
-    for tree in trees:
-        tree.draw()
 
     # Change color of bag slot if hovering over an option
     for slot in BAG:
