f1807b2536c328eff9eede1ce5062691f3c76759
Chris Thrasher <chris.j.thrasher@gmail.com>
2020-09-26 16:17:36 -0600

    Reintroduced Tree class
    Simplies how trees are made since they're just collidable circles
    with a known radius and color.

--- course.py
+++ course.py
@@ -8,6 +8,12 @@ from vec2 import Vec2
 
 HOLE1 = pygame.image.load(os.path.join("assets", "Hole1.png"))
 
+class Tree(Circle):
+    def __init__(self, pos):
+        super().__init__(pos, 5, color.DARK_GREEN)
+    def draw(self):
+        super().draw()
+
 class Tee:
     def __init__(self, pos):
         self.pos = pos
@@ -53,27 +59,27 @@ class Course:
         self.holes[self.current_hole - 1].draw()
 
 tee1 = Tee(Vec2(390, 480))
-trees1 = [Circle(Vec2(450, 300), 5, color.DARK_GREEN),
-          Circle(Vec2(400, 350), 5, color.DARK_GREEN),
-          Circle(Vec2(350, 300), 5, color.DARK_GREEN)]
+trees1 = [Tree(Vec2(450, 300)),
+          Tree(Vec2(400, 350)),
+          Tree(Vec2(350, 300))]
 basket1 = Circle(Vec2(440, 160), 10, color.GREY)
 hole1 = Hole(HOLE1, tee1, trees1, basket1)
 
 tee2 = Tee(Vec2(390, 480))
-trees2 = [Circle(Vec2(440, 200), 5, color.DARK_GREEN),
-          Circle(Vec2(420, 250), 5, color.DARK_GREEN),
-          Circle(Vec2(400, 300), 5, color.DARK_GREEN),
-          Circle(Vec2(380, 350), 5, color.DARK_GREEN),
-          Circle(Vec2(360, 400), 5, color.DARK_GREEN)]
+trees2 = [Tree(Vec2(440, 200)),
+          Tree(Vec2(420, 250)),
+          Tree(Vec2(400, 300)),
+          Tree(Vec2(380, 350)),
+          Tree(Vec2(360, 400))]
 basket2 = Circle(Vec2(440, 160), 10, color.GREY)
 hole2 = Hole(HOLE1, tee2, trees2, basket2)
 
 tee3 = Tee(Vec2(390, 480))
-trees3 = [Circle(Vec2(440, 200), 5, color.DARK_GREEN),
-          Circle(Vec2(420, 250), 5, color.DARK_GREEN),
-          Circle(Vec2(400, 300), 5, color.DARK_GREEN),
-          Circle(Vec2(380, 250), 5, color.DARK_GREEN),
-          Circle(Vec2(360, 200), 5, color.DARK_GREEN)]
+trees3 = [Tree(Vec2(440, 200)),
+          Tree(Vec2(420, 250)),
+          Tree(Vec2(400, 300)),
+          Tree(Vec2(380, 250)),
+          Tree(Vec2(360, 200))]
 basket3 = Circle(Vec2(440, 160), 10, color.GREY)
 hole3 = Hole(HOLE1, tee3, trees3, basket3)
 
